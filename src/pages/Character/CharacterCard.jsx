4c166fc35e5b4771099207eda25e268dc1ecaad7
Dan Zerbib <danzerbib@gmail.com>
2026-03-20 23:35:51 +0100

    Styled add to favorite button + wip add to favorites logic

--- src/pages/Character/CharacterCard.jsx
+++ src/pages/Character/CharacterCard.jsx
@@ -8,25 +8,33 @@ import { useState } from 'react';
 // Modules yarn
 import Cookies from 'js-cookie';
 import { Link } from 'react-router';
+import { CiHeart } from 'react-icons/ci';
 
 const CharacterCard = ({ character }) => {
-    const [favorites, setFavorites] = useState(JSON.parse(Cookies.get('characters_favorites')));
+    const charactersFavorites = Cookies.get('characters_favorites');
+    const initFavorites = charactersFavorites ? JSON.parse(charactersFavorites) : [];
+    console.log('charactersFavorites', JSON.parse(charactersFavorites));
+
+    const [favorites, setFavorites] = useState(initFavorites);
 
     return (
         <>
             <article className="character-card">
-                {/* <Button
-                    text="COEUR"
+                <Button
+                    text={<CiHeart />}
+                    className="add-to-favorite"
                     onClickFunc={() => {
                         // Ajouter le nouveau favori
                         if (!favorites.includes(character._id)) {
+                            console.log('favorites', favorites);
                             const copyFavorites = [...favorites];
                             copyFavorites.push(character._id);
-                            setFavorites(copyFavorites);
                             Cookies.set('characters_favorites', JSON.stringify(copyFavorites));
+                            console.log('copyFavorites', copyFavorites);
+                            setFavorites(copyFavorites);
                         }
                     }}
-                /> */}
+                />
 
                 <Link to={`/character/${character._id}`}>
                     <h2>{character.name}</h2>
