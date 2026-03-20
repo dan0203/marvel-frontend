3bd7bb139e97ecabd0cbfc0b5225bf3671b62253
Dan Zerbib <danzerbib@gmail.com>
2026-03-20 22:42:43 +0100

    Added links to Home page cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import './Home.css';
 import hero from '../../assets/images/hero.jpg';
 import characters from '../../assets/images/characters.jpg';
 import comics from '../../assets/images/comics.jpg';
+// Modules yarn
+import { Link } from 'react-router';
 
 const Home = () => {
     return (
@@ -17,15 +19,19 @@ const Home = () => {
                 </h1>
 
                 <section>
-                    <article className="card">
-                        <h2>Voir les personnages</h2>
-                        <img src={characters} alt="Voir les personnages MARVEL" />
-                    </article>
+                    <Link to="/characters">
+                        <article className="card">
+                            <h2>Voir les personnages</h2>
+                            <img src={characters} alt="Voir les personnages MARVEL" />
+                        </article>
+                    </Link>
 
-                    <article className="card">
-                        <h2>Voir les comics</h2>
-                        <img src={comics} alt="Voir les comics MARVEL" />
-                    </article>
+                    <Link to="/comics">
+                        <article className="card">
+                            <h2>Voir les comics</h2>
+                            <img src={comics} alt="Voir les comics MARVEL" />
+                        </article>
+                    </Link>
                 </section>
             </div>
         </main>
